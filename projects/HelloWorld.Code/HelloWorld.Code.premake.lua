aba1957f90b63bd0dbccf803cd671e7652450fd0
Vivien Millet <vivien.millet@gmail.com>
2021-03-02 15:07:26 +0100

    [premake] update sample

--- projects/HelloWorld.Code/HelloWorld.Code.premake.lua
+++ projects/HelloWorld.Code/HelloWorld.Code.premake.lua
@@ -2,7 +2,7 @@
 Phantom_console_app("HelloWorld.Code",  { "Phantom.Code" } -- DEPENDENCIES
 	,
 	function(Vars)  -- include private
-
+		files { "projects/HelloWorld.Code/**.cpplite" }
 	end
 	,
 	function(Vars) -- link
